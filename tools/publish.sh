fc3efb22a98c9bf51e4a6d757d4aa4f6bf83d945
Oliver Kaufmann <okaufmann91@gmail.com>
2020-02-03 00:43:24 +0100

    add crx3 publish script

---
+++ tools/publish.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")/.."
+
+newVersion=$1
+echo "publishing new version $newVersion"
+
+echo "cleaning up"
+rm -Rf dist
+rm -Rf .temp/*
+
+echo "building"
+yarn prod
+
+echo "publishing"
+npx crx3 dist --zip=.temp/extension.crx \
+    --key extension.pem \
+    --appVersion "$newVersion" \
+    --browserVersion 70.0.0 \
+    --xml .temp/extension.xml \
+    --crxURL "https://dobby.mighty-code.com/storage/extension.crx"
+scp .temp/extension.crx forge@forge-01.mighty-code.com:dobby.mighty-code.com/storage/app/public
+scp .temp/extension.xml forge@forge-01.mighty-code.com:dobby.mighty-code.com/storage/app/public
+
+rm dist.crx
